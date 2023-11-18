b9f560b232eb6513f39e576e629f56d589be621f
umdevadriano <adrianosilvasampaio86@gmail.com>
2023-11-18 02:13:58 -0300

    validação exclusão do arquivo imagem do produto

--- src/app/useCases/Products/deleteProduct.ts
+++ src/app/useCases/Products/deleteProduct.ts
@@ -1,10 +1,18 @@
 import { Product } from './../../models/Product';
+import fs from 'fs';
+import path from 'node:path';
 
 import { Request, Response } from 'express';
 export async function deleteProduct(req : Request ,res: Response){
   try{
     const {productId} = req.params;
-    await Product.findByIdAndDelete(productId);
+    const productUpd = await Product.findByIdAndDelete(productId);
+    const imagemDeleted =productUpd?.imgePath  ;
+    if(imagemDeleted){
+      const uploads = path.resolve('.',`uploads/${imagemDeleted}`) ;
+      fs.unlink(`${uploads}`,(err)=>{if (err) throw err;});
+    }
+
     res.sendStatus(204);
   }catch(error){
     res.sendStatus(500);
