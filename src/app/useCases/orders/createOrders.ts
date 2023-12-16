feb22e9378c40be83f65800dc2f6cf123e39f900
umdevadriano <adrianosilvasampaio86@gmail.com>
2023-12-16 02:22:38 -0300

    implementando socket io

--- src/app/useCases/orders/createOrders.ts
+++ src/app/useCases/orders/createOrders.ts
@@ -1,3 +1,4 @@
+import { io } from '../../..';
 import { Order } from './../../models/Order';
 import { Request, Response } from 'express';
 
@@ -6,6 +7,8 @@ export async function createOrders(req : Request ,res: Response){
   try{
     const {table,products} = req.body;
     const order  = await Order.create({table,products});
+    const orderDetails = await order.populate('products.product');
+    io.emit('order@new',orderDetails);
     res.status(201).json(order);
   }catch(error){
     console.log(error);
--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,29 @@ import path from 'node:path';
 import express from 'express';
 import mongoose from 'mongoose';
 import { router } from './router';
+import http from 'node:http';
+import { Server } from 'socket.io';
+const app  = express() ;
+const server = http.createServer(app);
+export const io = new Server(server);
 
 
 mongoose.connect('mongodb://127.0.0.1:27017/adriano').then(()=>{
-  const app  = express() ;
+
   const porta : number = 3001;
-  app.use('/uploads',express.static(path.resolve(__dirname,'..','uploads')))
+
+
+  app.use((req,res,next)=>{
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Methods','*');
+    res.setHeader('Access-Control-Allow-Headers','*');
+    next();
+  });
+  app.use('/uploads',express.static(path.resolve(__dirname,'..','uploads')));
   app.use(express.json());
   app.use(router);
 
-  app.listen(porta,()=>{
+  server.listen(porta,()=>{
     console.log(`server rodando na porta: http://localhost:${porta}`);
   });
 }).catch(()=> console.log('erro ao conectar ao mongo db'));
